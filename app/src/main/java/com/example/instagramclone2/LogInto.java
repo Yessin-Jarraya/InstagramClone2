3e71cba6eec361cc26acdfc8ff97ce844d0e23b5
29163444 <yessinjarraya1025@gmail.com>
2022-09-02 09:40:07 +0100

    Added something

--- app/src/main/java/com/example/instagramclone2/LogInto.java
+++ app/src/main/java/com/example/instagramclone2/LogInto.java
@@ -1,14 +1,21 @@
 package com.example.instagramclone2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInto extends AppCompatActivity implements View.OnClickListener {
+
     private EditText edtLogInName,edtLogInPassword;
     private Button btnSignUp2,btnLogIn2;
 
@@ -16,21 +23,42 @@ public class LogInto extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginto);
+        setTitle("LogIn");
         edtLogInName = findViewById(R.id.edtLogInName);
         edtLogInPassword = findViewById(R.id.edtLogInPassword);
-        btnSignUp2 = findViewById(R.id.btnLogIn2);
+        btnSignUp2 = findViewById(R.id.btnSignUp2);
         btnLogIn2 = findViewById(R.id.btnLogIn2);
         btnSignUp2.setOnClickListener(this);
         btnLogIn2.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnSignUp2:
+                Intent intent = new Intent(LogInto.this,MainActivity.class);
+                startActivity(intent);
+                finish();
 
                 break;
             case R.id.btnLogIn2:
+                ParseUser.logInInBackground(edtLogInName.getText().toString(), edtLogInPassword.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user!=null && e==null){
+                            FancyToast.makeText(LogInto.this,  " You Logged in successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+
+
+                        }
+                        else {                            FancyToast.makeText(LogInto.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+
+                        }
+                    }
+
+                });
+
                 break;
         }
 
--- app/src/main/java/com/example/instagramclone2/MainActivity.java
+++ app/src/main/java/com/example/instagramclone2/MainActivity.java
@@ -2,17 +2,22 @@ package com.example.instagramclone2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
-public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public   class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText edtSignUpName, edtSignUpPassword, edtSignUpEmail;
     private Button btnSignUp1, btnLogIn1;
 
@@ -20,15 +25,20 @@ public abstract class MainActivity extends AppCompatActivity implements View.OnC
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("SignUp");
         edtSignUpName = findViewById(R.id.edtSignUpName);
         edtSignUpPassword = findViewById(R.id.edtSignUpPassword);
         edtSignUpEmail = findViewById(R.id.edtSignUpEmail);
-        btnSignUp1 = findViewById(R.id.btnSignUp2);
-        btnLogIn1 = findViewById(R.id.btnLogIn2);
+        btnSignUp1 = findViewById(R.id.btnSignUp1);
+        btnLogIn1 = findViewById(R.id.btnLogIn1);
         btnSignUp1.setOnClickListener(MainActivity.this);
         btnLogIn1.setOnClickListener(MainActivity.this);
 
-    }
+
+
+        }
+
+
 
     @Override
     public void onClick(View view) {
@@ -42,7 +52,10 @@ public abstract class MainActivity extends AppCompatActivity implements View.OnC
                     @Override
                     public void done(ParseException e) {
                         if (e==null){
-                            FancyToast.makeText(MainActivity.this,  appUser.get("username")+" is signed up succesfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                            FancyToast.makeText(MainActivity.this,  appUser.get("username")+" is signed up successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                            Intent intent = new Intent(MainActivity.this,LogInto.class);
+                            startActivity(intent);
+                            finish();
 
                         }
                         else{
@@ -57,6 +70,10 @@ public abstract class MainActivity extends AppCompatActivity implements View.OnC
 
                 break;
             case R.id.btnLogIn1:
+                Intent intent = new Intent(MainActivity.this,LogInto.class);
+                startActivity(intent);
+                finish();
+
                 break;
         }
     }

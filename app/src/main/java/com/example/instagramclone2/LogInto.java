d2b0274ff2da5df9b111648091e8b12bb5a4034f
29163444 <yessinjarraya1025@gmail.com>
2022-09-03 14:54:48 +0100

    New activity

--- app/src/main/java/com/example/instagramclone2/LogInto.java
+++ app/src/main/java/com/example/instagramclone2/LogInto.java
@@ -2,7 +2,9 @@ package com.example.instagramclone2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,8 +18,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInto extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText edtLogInName,edtLogInPassword;
-    private Button btnSignUp2,btnLogIn2;
+    private EditText edtLogInName, edtLogInPassword;
+    private Button btnSignUp2, btnLogIn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +30,18 @@ public class LogInto extends AppCompatActivity implements View.OnClickListener {
         edtLogInPassword = findViewById(R.id.edtLogInPassword);
         btnSignUp2 = findViewById(R.id.btnSignUp2);
         btnLogIn2 = findViewById(R.id.btnLogIn2);
+        edtLogInPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    onClick(btnLogIn2);
+                }
+                return false;
+            }
+        });
         btnSignUp2.setOnClickListener(this);
         btnLogIn2.setOnClickListener(this);
-        if (ParseUser.getCurrentUser()!=null){
+        if (ParseUser.getCurrentUser() != null) {
             ParseUser.getCurrentUser().logOut();
         }
 
@@ -40,30 +51,52 @@ public class LogInto extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnSignUp2:
-                Intent intent = new Intent(LogInto.this,MainActivity.class);
+                Intent intent = new Intent(LogInto.this, MainActivity.class);
                 startActivity(intent);
                 finish();
 
                 break;
             case R.id.btnLogIn2:
-                ParseUser.logInInBackground(edtLogInName.getText().toString(), edtLogInPassword.getText().toString(), new LogInCallback() {
-                    @Override
-                    public void done(ParseUser user, ParseException e) {
-                        if (user!=null && e==null){
-                            FancyToast.makeText(LogInto.this,  " You Logged in successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                if (edtLogInName.getText().toString().equals("") || edtLogInPassword.getText().toString().equals("")) {
+                    FancyToast.makeText(LogInto.this, " Username, Password is required", FancyToast.LENGTH_SHORT, FancyToast.INFO, true).show();
+
+                } else {
+                    ParseUser.logInInBackground(edtLogInName.getText().toString(), edtLogInPassword.getText().toString(), new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+                            if (user != null && e == null) {
+                                FancyToast.makeText(LogInto.this, " You Logged in successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                                transitionToSocialMediaActivity();
+
+                            } else {
+                                FancyToast.makeText(LogInto.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
 
 
+                            }
                         }
-                        else {                            FancyToast.makeText(LogInto.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
 
+                    });
 
-                        }
-                    }
+                    break;
+                }
 
-                });
-
-                break;
         }
-
     }
+    public void rootLayoutTapped(View view){
+        try{
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    private void transitionToSocialMediaActivity() {
+        Intent intent = new Intent(LogInto.this,SocialMediaActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
+
+
+
--- app/src/main/java/com/example/instagramclone2/MainActivity.java
+++ app/src/main/java/com/example/instagramclone2/MainActivity.java
@@ -6,7 +6,9 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -31,25 +33,35 @@ public   class MainActivity extends AppCompatActivity implements View.OnClickLis
 
         edtSignUpName = findViewById(R.id.edtSignUpName);
         edtSignUpPassword = findViewById(R.id.edtSignUpPassword);
+        edtSignUpPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    onClick(btnSignUp1);
+                }
+                return false;
+            }
+        });
         edtSignUpEmail = findViewById(R.id.edtSignUpEmail);
         btnSignUp1 = findViewById(R.id.btnSignUp1);
         btnLogIn1 = findViewById(R.id.btnLogIn1);
         btnSignUp1.setOnClickListener(MainActivity.this);
         btnLogIn1.setOnClickListener(MainActivity.this);
         if (ParseUser.getCurrentUser()!=null){
-            ParseUser.getCurrentUser().logOut();
+            transitionToSocialMediaActivity();
+
+            }
         }
-
-
-
-        }
-
 
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnSignUp1:
+                if(edtSignUpEmail.getText().toString().equals("")||edtSignUpName.getText().toString().equals("")||edtSignUpPassword.getText().toString().equals("")){
+                    FancyToast.makeText(MainActivity.this,  " Email, Username, Password is required", FancyToast.LENGTH_SHORT, FancyToast.INFO, true).show();
+
+                } else  {
                 ParseUser appUser=new ParseUser();
                 appUser.setUsername(edtSignUpName.getText().toString());
                 appUser.setEmail(edtSignUpEmail.getText().toString());
@@ -59,20 +71,18 @@ public   class MainActivity extends AppCompatActivity implements View.OnClickLis
                     public void done(ParseException e) {
                         if (e==null){
                             FancyToast.makeText(MainActivity.this,  appUser.get("username")+" is signed up successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
-                            Intent intent = new Intent(MainActivity.this,LogInto.class);
-                            startActivity(intent);
-                            finish();
+                            transitionToSocialMediaActivity();
 
-                        }
-                        else{
+                        } else{
                             FancyToast.makeText(MainActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
 
-                        }
-                    }
+                        }}
+
+                    });
 
 
 
-                });
+                }
 
                 break;
             case R.id.btnLogIn1:
@@ -82,5 +92,18 @@ public   class MainActivity extends AppCompatActivity implements View.OnClickLis
 
                 break;
         }
+    }
+    public void rootLayoutTapped(View view){
+        try{
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+    }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    private void transitionToSocialMediaActivity() {
+        Intent intent = new Intent(MainActivity.this,SocialMediaActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
